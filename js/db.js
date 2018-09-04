335da9f46c6aa824e480b55852cea954290e64ec
harlleimazetti <harlleimazetti@users.noreply.github.com>
2018-09-04 15:14:46 -0300

    Add files via upload

--- js/db.js
+++ js/db.js
@@ -1,76 +1,76 @@
-var db = openDatabase ("frpmmobile", "1.0", "FRPM Mobile", 65535);
-db.transaction (function (transaction) 
+var db = openDatabase ("favmobile", "1.0", "FAV Mobile", 65535);
+db.transaction (function (transaction)
 {
 	console.log('Configurando Banco de Dados...');
 
 	//var sql = "DROP TABLE config";
 	//transaction.executeSql (sql, undefined, function() { }, error);
-	
+
 	var sql = "CREATE TABLE IF NOT EXISTS config " +
 		" (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
 		"url_servidor VARCHAR(200), " +
 		"id_pessoa INTEGER, " +
-		"nome VARCHAR(100), " + 
-		"grupo VARCHAR(100) " + 
+		"nome VARCHAR(100), " +
+		"grupo VARCHAR(100) " +
 		")"
 	transaction.executeSql (sql, undefined, function() { }, error);
 	//console.log(sql);
-	
-	var sql = "INSERT OR IGNORE INTO config (id, url_servidor) VALUES ('1', 'http://www.frpm.com.br/frpmmobile/sincronizar.php') ";
+
+	var sql = "INSERT OR IGNORE INTO config (id, url_servidor) VALUES ('1', 'http://www.fav-uninorte.com.br/favmobile/sincronizar.php') ";
 	transaction.executeSql (sql, undefined, function() { }, error);
 	//console.log(sql);
-	
+
 	//var sql = "DROP TABLE pessoas";
 	//transaction.executeSql (sql, undefined, function() { }, error);
-	
+
 	var sql = "CREATE TABLE IF NOT EXISTS pessoas " +
 		" (id_pessoa INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
 		"id_grupo INTEGER, " +
 		"id_tipo INTEGER, " +
 		"id_cargo INTEGER, " +
 		"id_funcao INTEGER, " +
-		"id_departamento INTEGER, " + 
-		"id_estado_lotacao INTEGER, " + 
-		"ativo VARCHAR(1), " + 
+		"id_departamento INTEGER, " +
+		"id_estado_lotacao INTEGER, " +
+		"ativo VARCHAR(1), " +
 		"acesso VARCHAR(1), " +
-		"nome VARCHAR(100), " + 
-		"nascimento DATE, " + 
-		"razao_social VARCHAR(100), " + 
-		"nome_fantasia VARCHAR(100), " + 
-		"cnpj VARCHAR(15), " + 
-		"inscricao VARCHAR(20), " + 
-		"cpf VARCHAR(15), " + 
+		"nome VARCHAR(100), " +
+		"nascimento DATE, " +
+		"razao_social VARCHAR(100), " +
+		"nome_fantasia VARCHAR(100), " +
+		"cnpj VARCHAR(15), " +
+		"inscricao VARCHAR(20), " +
+		"cpf VARCHAR(15), " +
 		"admissao DATE, " +
 		"tempo_servico VARCHAR(2), " +
-		"ci VARCHAR(30), " + 
-		"site VARCHAR(100), " + 
-		"obs TEXT " + 
+		"ci VARCHAR(30), " +
+		"site VARCHAR(100), " +
+		"obs TEXT " +
 		")";
 	transaction.executeSql (sql, undefined, function() { }, error);
 	//console.log(sql);
-	
+
 	//var sql = "DROP TABLE contas";
 	//transaction.executeSql (sql, undefined, function() { }, error);
-	
+
 	var sql = "CREATE TABLE IF NOT EXISTS contas " +
 		" (id_conta INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
 		"id_pessoa INTEGER, " +
 		"numero VARCHAR(30), " +
 		"data_abertura DATE, " +
 		"saldo_inicial REAL(10,2), " +
-		"obs TEXT " + 
+		"obs TEXT " +
 		")";
 	transaction.executeSql (sql, undefined, function() { }, error);
 	//console.log(sql);
-	
+
 	//var sql = "DROP TABLE lancamentos";
 	//transaction.executeSql (sql, undefined, function() { }, error);
-	
+
 	var sql = "CREATE TABLE IF NOT EXISTS lancamentos " +
 		" (id_lancamento INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
 		"id_conta INTEGER, " +
-		"id_tabela INTEGER, " + 
-		"data DATE, " + 
+		"id_tabela INTEGER, " +
+		"data DATE, " +
 		"descricao VARCHAR(200), " +
 		"dc VARCHAR(1), " +
 		"valor REAL(10,2) " +
@@ -83,7 +83,7 @@ function ok ()
 {
 }
 
-function error (transaction, err) 
+function error (transaction, err)
 {
 	console.log("Erro no banco de dados: " + err.message);
 	return false;
